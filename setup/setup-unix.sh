66b0d88717b6e473c9a7224489ef053294d51f9a
CrusherNotDrip <90648119+CrusherNotDrip@users.noreply.github.com>
2024-09-03 19:44:30 -0400

    volume mixer fix + changed some haxelib versions

--- setup/setup-unix.sh
+++ setup/setup-unix.sh
@@ -3,15 +3,15 @@
 #
 # REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
 # https://haxe.org/download/version/4.2.5/
-haxelib install lime
-haxelib install openfl
-haxelib install flixel
-haxelib install flixel-addons
-haxelib install flixel-ui
-haxelib install flixel-tools
-haxelib install SScript 7.7.0
-haxelib install hxvlc
-haxelib install tjson
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.3
+haxelib install flixel 5.6.1
+haxelib install flixel-addons 3.2.2
+haxelib install flixel-ui 2.6.1
+haxelib install flixel-tools 1.5.1
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+haxelib install hxdiscord_rpc 1.2.0
+haxelib install hxvlc 1.8.2
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib install SScript 7.7.0
+haxelib install tjson 1.4.0
--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -3,17 +3,17 @@ color 0a
 cd ..
 @echo on
 echo Installing dependencies.
-haxelib install lime
-haxelib install openfl
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.3
 haxelib install flixel 5.6.1
-haxelib install flixel-addons 3.2.1
-haxelib install flixel-ui 2.5.0
-haxelib install flixel-tools
-haxelib install SScript 7.7.0
-haxelib install hxvlc
-haxelib install tjson
+haxelib install flixel-addons 3.2.2
+haxelib install flixel-ui 2.6.1
+haxelib install flixel-tools 1.5.1
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+haxelib install hxdiscord_rpc 1.2.0
+haxelib install hxvlc 1.8.2
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib install SScript 7.7.0
+haxelib install tjson 1.4.0
 echo Finished!
 pause
