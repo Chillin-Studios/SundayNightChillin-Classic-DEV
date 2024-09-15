cb39b6f5fbe006c44ad74e96a29c914f3f57eeb0
Til Tjardes <til.tjardes@gmail.com>
2024-09-14 22:04:19 -0600

    was the last push on the 9th of this month? (crusher please dont mind this)

---
+++ source/states/GalleryState.hx
@@ -0,0 +1,28 @@
+package states;
+
+class GalleryState extends MusicBeatState
+{
+    override public function create():Void
+    {
+        var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuBG'));
+		bg.antialiasing = ClientPrefs.data.antialiasing;
+		bg.color = 0xFF818181;
+		bg.scrollFactor.set();
+		bg.screenCenter();
+		add(bg);
+
+		if (TitleState.titleJSON.checkerData.enabled)
+		{
+			var checkeredBG:Checkers = new Checkers({
+				size: TitleState.titleJSON.checkerData.size,
+				colors: [0x33FFFFFF, 0x0],
+				speed: TitleState.titleJSON.checkerData.speed,
+				alpha: 1
+			});
+			checkeredBG.scrollFactor.set(0, 0);
+			add(checkeredBG);
+		}
+
+        super.create();
+    }
+}
